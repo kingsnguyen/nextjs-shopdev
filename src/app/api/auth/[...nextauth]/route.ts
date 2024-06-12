d24a10660d0918630bb65c234c002832c20dbf55
Van Nguyen <nguyenit182@gmail.com>
2024-06-13 01:59:25 +0700

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,34 @@
-import { mergeAnonymousCartIntoUserCart } from "@/src/lib/db/cart";
-import { NextAuthOptions } from "next-auth";
+import { mergeAnonymousCartIntoUserCart } from "@/src/lib/db/cart"; 
+import  prisma  from "../../../../lib/db/prisma"
+import { env } from "../../../../lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/src/lib/db/prisma";
-import { env } from "@/src/lib/env";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
-import NextAuth from "next-auth";
 import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: NextAuthOptions  = {
-    adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
-    providers: [
-        GoogleProvider({
-            clientId: env.GOOGLE_CLIENT_ID!,
-            clientSecret: env.GOOGLE_CLIENT_SECRET!
-          })
-    ],
-    callbacks: {
-        session({ session, user}) {
-            session.user.id = user.id
-            return session
-        }
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
     },
-    events: {
-        async signIn({ user }) {
-          await mergeAnonymousCartIntoUserCart(user.id);
-        },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonymousCartIntoUserCart(user.id);
     },
-}
+  },
+};
 
-const handler =  NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export { handler as GET , handler as POST }
+export { handler as GET, handler as POST };
